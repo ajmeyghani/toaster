7f79f8b69c37843e632848955e3d770598d368a5
AJ Meyghani <meyghania@gmail.com>
2020-02-28 00:13:42 -0800

    try umd example

--- gh-pages/main.js
+++ gh-pages/main.js
@@ -1,6 +1,8 @@
-import toaster, { useToaster } from "../src/index.js";
+// import toaster, { useToaster } from "../src/index.js";
 
 // window.toaster = window.ajmtoaster.default;
+// window.toaster = window.ajmtoaster.useToaster({ injectCss: true, animation: "slide-down", theme: "dark" });
+window.toaster = window.ajmtoaster.useToaster({ injectCss: false, theme: "default" })
 
 // window.toaster = toaster;
 // window.toaster = useToaster();
